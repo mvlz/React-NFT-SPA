9ad2233a9ac87c5c9c13ecc28e1ffdb478852ea3
Marzieh Valizadeh <sureh1937@gmail.com>
2022-03-12 14:58:16 +0330

    feat: Delete saved items, Add saved to cart

--- src/pages/SavedPage/Saved.jsx
+++ src/pages/SavedPage/Saved.jsx
@@ -2,11 +2,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { Iconly } from "react-iconly";
 import "./Saved.css";
 import { deleteSaved } from "../../Components/redux/saved/savedAction";
+import { addCart } from "../../Components/redux/cart/cartAction";
 const Saved = () => {
   const { saved } = useSelector((state) => state.saved);
   const dispatch = useDispatch();
   const deleteHandler = (id) => {
     dispatch(deleteSaved(id));
+  };
+  const { cart } = useSelector((state) => state.cart);
+
+  const addToCartHandler = (product) => {
+    dispatch(addCart(product));
   };
   return (
     <div className="savedSection">
@@ -23,10 +29,18 @@ const Saved = () => {
                   <p>{item.asset_contract.schema_name}</p>
                   <p>cost</p>
                 </div>
-                <button className="buyNow">Buy Now</button>
+                <button
+                  className="buyNow"
+                  onClick={() => addToCartHandler(item)}
+                >
+                  Buy Now
+                </button>
               </div>
-              <button className="savedBtn">
-                <Iconly set="light" name="Star" stroke="light" />
+              <button
+                className="savedBtn"
+                onClick={() => deleteHandler(item.id)}
+              >
+                <Iconly set="bold" name="Star" stroke="light" />
               </button>
             </div>
           );
