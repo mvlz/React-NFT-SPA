ee2022432a81a0ec47952ab66fbbbc64cc112db8
Marzieh Valizadeh <sureh1937@gmail.com>
2022-03-15 22:15:49 +0330

    develop header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ThemeContext, themes } from "../theme/themeContext";
+import { Iconly } from "react-iconly";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -7,20 +8,33 @@ const Header = () => {
   return (
     <header className="header">
       <img src={logo} alt="1NFT" className="logo" />
-
-      <ThemeContext.Consumer>
-        {({ changeTheme }) => (
-          <button
-            color="link"
-            onClick={() => {
-              setDarkMode(!darkMode);
-              changeTheme(darkMode ? themes.dark : themes.light);
-            }}
-          >
-            <span className="d-lg-none d-md-block">Switch mode</span>
-          </button>
-        )}
-      </ThemeContext.Consumer>
+      <div className="rightheader">
+        <Iconly
+          set="light"
+          name="Notification"
+          primaryColor="black"
+          stroke="bold"
+        />
+        <Iconly set="light" name="Chat" primaryColor="black" stroke="bold" />
+        <ThemeContext.Consumer>
+          {({ changeTheme }) => (
+            <button
+              className="darkModeBtn"
+              onClick={() => {
+                setDarkMode(!darkMode);
+                changeTheme(darkMode ? themes.dark : themes.light);
+              }}
+            >
+              <span
+                className={darkMode ? "sun" : "moon"}
+                // style={{
+                //   boxShadow: "inset 0px 10px #999, inset 0px 4px 1px 1px #999",
+                // }}
+              ></span>
+            </button>
+          )}
+        </ThemeContext.Consumer>
+      </div>
     </header>
   );
 };
