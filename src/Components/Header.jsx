21971fe8366c2f10f87385f878025fb4cb944e31
Marzieh Valizadeh <sureh1937@gmail.com>
2022-03-17 17:00:13 +0330

    change theme switcher icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -25,12 +25,39 @@ const Header = () => {
                 changeTheme(darkMode ? themes.dark : themes.light);
               }}
             >
-              <span
-                className={darkMode ? "sun" : "moon"}
-                // style={{
-                //   boxShadow: "inset 0px 10px #999, inset 0px 4px 1px 1px #999",
-                // }}
-              ></span>
+              {darkMode ? (
+                <svg viewBox="0 0 512 512" width="24" height="24">
+                  <path
+                    fill="#000"
+                    stroke="#000"
+                    stroke-linecap="round"
+                    stroke-miterlimit="10"
+                    stroke-width="32"
+                    d="M256 48v48M256 416v48M403.08 108.92l-33.94 33.94M142.86 369.14l-33.94 33.94M464 256h-48M96 256H48M403.08 403.08l-33.94-33.94M142.86 142.86l-33.94-33.94"
+                  ></path>
+                  <circle
+                    cx="256"
+                    cy="256"
+                    r="80"
+                    fill="none"
+                    stroke="#000"
+                    stroke-linecap="round"
+                    stroke-miterlimit="10"
+                    stroke-width="32"
+                  ></circle>
+                </svg>
+              ) : (
+                <svg viewBox="0 0 512 512" width="24" height="24">
+                  <path
+                    d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
+                    fill="none"
+                    stroke="#000"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="32"
+                  ></path>
+                </svg>
+              )}
             </button>
           )}
         </ThemeContext.Consumer>
